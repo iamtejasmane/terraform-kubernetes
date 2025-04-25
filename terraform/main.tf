e02f15633d5f9c3439e743b55b8694f663d42a6d
Tejas Mane <tejasmane485@gmail.com>
2025-04-25 12:17:30 +0530

    frontend deployment

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,10 +5,44 @@ resource "kubernetes_service" "webapp-service" {
       name = "webapp-service"
     }
   spec {
+    selector = {
+     name = "webapp" 
+    }
     port {
       port = 8080
       node_port = 30080
     }
     type = "NodePort"
   }
+}
+
+# Frontend app deployment 
+resource "kubernetes_deployment" "frontend" {
+  metadata {
+    name = "frontend"
+    labels = {
+      "name" = "frontend"
+    }
+  }
+  spec {
+    replicas = 4
+    template {
+        metadata {
+          name = "webapp"
+          labels = {
+            "name" = "webapp"
+          }
+        }
+      spec {
+        container {
+          image = "kodekloud/webapp-color:v1"
+          name = "simple-webapp"
+          port {
+            container_port = 8080
+          }
+        }
+      }
+    }
+  }
+
 }
