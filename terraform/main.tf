df8116780aea009203411aecce6e0a05dbd32998
Tejas Mane <tejasmane485@gmail.com>
2025-04-25 12:05:31 +0530

    webapp-service

---
+++ terraform/main.tf
@@ -0,0 +1,14 @@
+# Kubernetes Service
+
+resource "kubernetes_service" "webapp-service" {
+    metadata {
+      name = "webapp-service"
+    }
+  spec {
+    port {
+      port = 8080
+      node_port = 30080
+    }
+    type = "NodePort"
+  }
+}
