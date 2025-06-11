f4c28b19d224f63301e04ac61f86c042d6710873
Tejas Mane <tejasmane485@gmail.com>
2025-06-11 09:19:46 +0530

    private network added

--- LAMP-stack-deployment/terraform/main.tf
+++ LAMP-stack-deployment/terraform/main.tf
@@ -1,31 +1,31 @@
 # PHP image
 resource "docker_image" "php-httpd-image" {
-    name = "php-httpd:challenge"
-    build {
-      path = "lamp_stack/php_httpd"
-      label = {
-        challenge = "second"
-      }
+  name = "php-httpd:challenge"
+  build {
+    path = "lamp_stack/php_httpd"
+    label = {
+      challenge = "second"
     }
+  }
 }
 
 # MariaDB image
 
 resource "docker_image" "mariadb-image" {
-    name = "mariadb:challenge"
-    build {
-      path = "lamp_stack/custom_db"
-      label = {
-        challenge = "second"
-      }
+  name = "mariadb:challenge"
+  build {
+    path = "lamp_stack/custom_db"
+    label = {
+      challenge = "second"
     }
+  }
 }
 
 # Docker container for php webserver
 resource "docker_container" "php-httpd" {
-  name = "webserver"
+  name     = "webserver"
   hostname = "php-httpd"
-  image = "php-httpd:challenge"
+  image    = "php-httpd:challenge"
   labels {
     label = "challenge"
     value = "second"
@@ -36,13 +36,18 @@ resource "docker_container" "php-httpd" {
     internal = "80"
   }
 
+  volumes {
+    host_path = "/root/code/terraform-challenges/challenge2/lamp_stack/website_content/"
+    container_path = "/var/www/html"
+  }
+
 }
 
 # Docker container for php webserver
 resource "docker_container" "phpmyadmin" {
-  name = "db_dashboard"
+  name     = "db_dashboard"
   hostname = "phpmyadmin"
-  image = "phpmyadmin/phpmyadmin"
+  image    = "phpmyadmin/phpmyadmin"
   labels {
     label = "challenge"
     value = "second"
@@ -52,15 +57,17 @@ resource "docker_container" "phpmyadmin" {
     external = "8081"
     internal = "80"
   }
-  links = [ "db", "db_dashboard" ]
+  depends_on = [ docker_container.mariadb ]
+  # deprecated
+  links = ["db", "db_dashboard"]
 
 }
 
 # Docker container for mariadb
 resource "docker_container" "mariadb" {
-  name = "db"
+  name     = "db"
   hostname = "db"
-  image = "mariadb:challenge"
+  image    = "mariadb:challenge"
   labels {
     label = "challenge"
     value = "second"
@@ -70,18 +77,25 @@ resource "docker_container" "mariadb" {
     external = "3306"
     internal = "3306"
   }
-  env = [ "MYSQL_ROOT_PASSWORD=1234", "MYSQL_DATABASE=simple-website" ]
-  links = [ "db", "db_dashboard" ]
+  env   = ["MYSQL_ROOT_PASSWORD=1234", "MYSQL_DATABASE=simple-website"]
+  links = ["db", "db_dashboard"]
 
   volumes {
-    volume_name = "mariadb-volume"
+    volume_name    = docker_volume.mariadb_volume.name
     container_path = "/var/lib/mysql"
   }
-
-  depends_on = [ docker_container.phpmyadmin ]
 
 }
 
 resource "docker_volume" "mariadb_volume" {
   name = "mariadb-volume"
+}
+
+resource "docker_network" "private_network" {
+  name = "my_network"
+  attachable = true
+  labels {
+    label = "challenge"
+    value = "second"
+  }
 }
